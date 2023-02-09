5fe4ee1eb3b7913334d8467c3a02a82bcbe536ef
alwinabraham <alwinabraham098@gmail.com>
2023-02-09 12:29:23 +0530

    api modified

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import { useState,useEffect } from 'react';
 
 function App() {
   const [name,setname] = useState("delhi")
+  const [renderName,setRenderName] = useState('')
+  const [temperature,setTemperature] = useState('')
+  const [icon,setIcon] = useState('')
+  const [description,setDescription] = useState('')
+  const [humidity,setHumidity] = useState('')
+  const [wind,setWind] = useState('')
   const [data,setData] = useState('')
 
   useEffect(() => {
@@ -13,25 +19,49 @@ function App() {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apikey}`)
         .then((response)=>{
           console.log(response);
-          let weatherData = [response.data.name]
-          setData(weatherData)
+          let renderNameData = response.data.name
+          let temp = response.data.main.temp
+          let newTemp = Math.round(temp - 273.15)
+          let weather = response.data.weather[0]
+          let icondata = weather.icon
+          let humiditydata = response.data.main.humidity
+          let descriptiondata = weather.description
+          let winddata = response.data.wind.speed
+          setRenderName(renderNameData)
+          setData(response.data.name)
+          setTemperature(newTemp)
+          setIcon(icondata)
+          setHumidity(humiditydata)
+          setDescription(descriptiondata)
+          setWind(winddata)
         })
         .catch((error)=>{
           console.log(error);
         })
     }
     weather(name)
-  })
+  },[name])
   
   
 
   return (
+    <>
     <div>
-    <form>
-    <input />
-    <button>o</button>
-    </form>
+    <input
+      type="text"
+      name='name'
+      placeholder='Enter your city'
+      value={name}
+      onChange={(e)=>setname(e.target.value)} />
     </div>
+    <div>
+      <h1>{renderName}</h1>
+      <h1>{temperature}C</h1>
+      <span><img src={`http://openweathermap.org/img/wn/${icon}.png`}/><h4>{description}</h4></span>
+      <h1>{humidity}</h1>
+      <h1>{wind}</h1>
+    </div>
+    </>
   );
 }
 
